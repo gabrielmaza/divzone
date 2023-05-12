3a93c7f2200117308a2875f3aa073191cb0682f1
gmazasilentium <80764590+gmazasilentium@users.noreply.github.com>
2023-05-12 20:23:12 -0300

    First commit

---
+++ src/pages/Home.page.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { SelectedPageEnum } from "@/shared/typesEnum";
+import Navbar from "@/components/nav-bar";
+import Main from "@/components/main"
+import Benefits from "@/components/benefits"
+import Services from "@/components/services";
+import ContactUs from "@/components/contact-us";
+import Footer from "@/components/footer";
+
+function Home() {
+
+  const [selectedPage, setSelectedPage] = useState<SelectedPageEnum>(SelectedPageEnum.Home)
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true)
+        setSelectedPage(SelectedPageEnum.Home)
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false)
+    }
+    window.addEventListener("scroll", handleScroll)
+    return () => removeEventListener("scroll", handleScroll)
+  }, [])
+
+
+  return (
+    <div className="bg-gray-950">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Main setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <Services setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home;
