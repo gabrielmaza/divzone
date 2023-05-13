5430b533bfc564545f9814a8d3348ef1045b8653
gmazasilentium <80764590+gmazasilentium@users.noreply.github.com>
2023-05-13 19:08:07 -0300

    nextjs project

--- divzone-landing/README.md
+++ divzone-landing/README.md
@@ -1,7 +1,3 @@
-# DivZone
-
-## React, Vite, Typescript and Tailwind css themes.
-
 ## Docs 👇
 
 **https://divzone-typescript-doc.vercel.app/docs/intro**
@@ -9,3 +5,36 @@
 ## Support
 
 **help@div.zone**
+
+<p align="center">
+  <img src="https://divzone-react-theme-docs.vercel.app/img/logo.svg" width="50" height="50" alt="Framer Motion Icon" />
+</p>
+<h1 align="center">DivZone</h1>
+<h3 align="center">
+    React, Vite, Typescript and Tailwind css themes.
+</h3>
+<h3 align="center">
+Experience revolutionary web templates with the latest design standards, cutting-edge technology, and unmatched performance.
+</h3>
+<br/>
+
+This repo contains the source code for [DivZone React Theme](https://github.com/gabrielmaza/divzone-react-theme)
+
+### 📚 Docs
+
+- Check out [our documentation](https://divzone-react-theme-docs.vercel.app/) for guides and a full API reference.
+- Or see [our live demo](https://divzone-react-theme.vercel.app/).
+
+### 👩🏻‍⚖️ License
+
+- DivZone is MIT licensed.
+
+## Support
+
+**help@div.zone**
+
+## Credits
+
+- Preview images: Kalcha Software, pngegg.com and pngwing.com
+- Fonts: “DM Sans” and “Montserrat” free google font
+- Free Font Based Icons by Heroicons
--- divzone-landing/tailwind.config.js
+++ divzone-landing/tailwind.config.js
@@ -1,18 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/static/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "gray-600": "#a299b9",
+        "gray-700": "#433E4F",
+        "gray-800": "#322E3C",
+        "gray-950": "#1F1D23",
+        "primary-100": "#dcccff",
+        "primary-300": "#8165be",
+        "primary-500": "#5e4e82",
+        "secondary-50": "#ffd5fc",
+        "secondary-100": "#ffb7f8",
+        "secondary-300": "#ef27db",
+        "secondary-400": "#a61998",
+        "secondary-500": "#5a0e52",
       },
+      backgroundImage: (theme) => ({
+        "mobile-home": "url('./img/HomePageGraphic.png')",
+      }),
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
     },
   },
   plugins: [],
-}
+};
