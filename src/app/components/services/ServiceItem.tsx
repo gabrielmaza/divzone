f78db2d92dab7b7fb511e83fc16a421c88127e02
gmazasilentium <80764590+gmazasilentium@users.noreply.github.com>
2023-05-19 18:35:05 -0300

    Change folder Fixes #26

---
+++ src/app/components/services/ServiceItem.tsx
@@ -0,0 +1,23 @@
+type Props = {
+    name: string;
+    description?: string;
+    background?: "secondary-100" | "primary-100"
+};
+
+export default function ServiceItem({ name, description }: Props) {
+    const overlayStyles = `p-5 absolute z-30 flex
+    h-full w-full flex-col items-center justify-center
+    whitespace-normal bg-primary-500 text-center text-white
+    opacity-0 transition duration-500 hover:opacity-90 rounded-2xl`;
+
+    return (
+        <li className="relative inline-block h-[280px] w-[280px] rounded-2xl bg-secondary-50">
+            <div className={overlayStyles}>
+                <p className="mt-5">{description}</p>
+            </div>
+            <div className="flex flex-col justify-center p-5 h-full w-full whitespace-break-spaces">
+                <p className="text-3xl font-bold text-gray-950 ">{name}</p>
+            </div>
+        </li>
+    );
+};
